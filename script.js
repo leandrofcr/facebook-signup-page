23700b4e2e72a245484c74f56337c68a4ed7825b
Leandro Reis <lleandrofr@gmail.com>
2021-03-22 16:11:18 -0300

    Requisito 7 - Botao entrar com javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonLogin = document.getElementById('button-login');
+buttonLogin.addEventListener('click', () => {
+  let userEmail = document.getElementById('user-email-phone').value;
+  alert(userEmail);
+});
