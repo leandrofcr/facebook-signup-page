59fcf1a71ecc4f21b77a7726fe8f24affb78e59e
Leandro Reis <lleandrofr@gmail.com>
2021-03-22 20:58:13 -0300

    Corrige posicionamento do formulario de login e inicia js do requisito 19

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ buttonLogin.addEventListener('click', () => {
   const userEmail = document.getElementById('user-email-phone').value;
   alert(userEmail);
 });
+
+const singUpBtn = document.getElementById('facebook-register');
+singUpBtn.addEventListener('click', () => {
+
+  const mainFormInput = document.getElementsByClassName('main-form');
+  for (let index = 0; index < mainFormInput.length; index += 1) {
+    if (mainFormInput[index].value === '') {
+      const newElement = document.createElement('p');
+      newElement.innerText = 'Campos inválidos';
+      document.querySelector('.form-registration').appendChild(newElement);  
+      console.log(mainFormInput[index]);   
+    }
+  }
+})
