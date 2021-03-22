945fc60712d5356998a68e0990b42632b2e051ae
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-22 19:21:31 -0300

    arruma exercicio 16

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ buttonLogin.addEventListener('click', () => {
   const userEmail = document.getElementById('user-email-phone').value;
   alert(userEmail);
 });
+
+
+const customRadio = document.getElementById('personalizado');
+const genderContainer = document.querySelector('.gender-container')
+  if (customRadio.prop('checked') === true) {
+    const newInput = document.createElement('input');
+    newInput.name = 'gender-custom';
+    newInput.placeholder = 'Gênero (opcional)';
+    genderContainer.appendChild(newInput);  
+  }
