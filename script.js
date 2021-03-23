d31ed63e9ec80d48307d3c5b2c3e10eaf704e258
Leandro Reis <lleandrofr@gmail.com>
2021-03-23 00:24:32 -0300

    Requisito 20 - Exibe texto de validacao

--- script.js
+++ script.js
@@ -28,14 +28,14 @@ singUpBtn.addEventListener('click', (event) => {
 });
 
 // Adiona um campo de texto se o gênero personalizado for selecionado
-const genderCustom = document.getElementById('personalizado');
-genderCustom.addEventListener('change', () => {
-  const newField = document.createElement('input');
-  newField.setAttribute('name', 'gender-custom');
-  newField.setAttribute('placeholder', 'Gênero (opcional)');
-  newField.setAttribute('id', 'gender-custom');
-  document.querySelector('.gender-container').appendChild(newField);
-});
+// const genderCustom = document.getElementById('personalizado');
+// genderCustom.addEventListener('change', () => {
+//   const newField = document.createElement('input');
+//   newField.setAttribute('name', 'gender-custom');
+//   newField.setAttribute('placeholder', 'Gênero (opcional)');
+//   newField.setAttribute('id', 'gender-custom');
+//   document.querySelector('.gender-container').appendChild(newField);
+// });
 
 // Remove o elemento do gênero personalizado caso outro gênero seja selecionado
 function removeCustomGender() {
@@ -45,3 +45,36 @@ const genderMale = document.getElementById('feminino');
 const genderFemale = document.getElementById('masculino');
 genderMale.addEventListener('click', removeCustomGender);
 genderFemale.addEventListener('click', removeCustomGender);
+
+// Exibe mensagem de validação
+
+function welcomeMessage() {
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const login = document.getElementById('phone-email').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const genders = document.getElementsByName('gender');
+  let gender = '';
+  for (let index = 0; index < genders.length; index += 1) {
+    if (genders[index].checked) {
+      gender = genders[index].value;
+      break;
+    }
+  }
+  return `Olá, ${firstName}${lastName}!${login}${birthdate}${gender}.`;
+}
+
+function validationPassed() {
+  const mainFormInput = document.getElementsByClassName('main-form');
+  for (let index = 0; index < mainFormInput.length; index += 1) {
+    if (mainFormInput[index].value !== '') {
+      const message = welcomeMessage();
+
+      const newEl = document.createElement('p');
+      newEl.innerText = message;
+      document.querySelector('.right-content').innerHTML = message;
+    }
+  }
+}
+
+singUpBtn.addEventListener('click', validationPassed);
